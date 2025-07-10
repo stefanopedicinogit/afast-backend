a440e449d066b491ca8bcfa3db1bc55f162767f7
PEDICINO STEFANO <stefano.pedicino@esternibisp.com>
2025-07-10 02:29:06 +0200

    AFAST V1.00

--- index.js
+++ index.js
@@ -9,15 +9,47 @@ app.use(express.json());
 
 const port = 8000;
 
-app.get('/test', async (req, res) => {
-    try {
-        const connection = await connect();
-        const result = await connection.execute('SELECT * FROM afast_results');
-        res.json({ result: result[0].map(row => row) });
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+app.get('/get', async (req, res) => {
+  try {
+    const connection = await connect();
+    const result = await connection.execute('SELECT * FROM afast_results');
+
+    console.log(result[0]);
+    const sum_credito_percentage = result[0].reduce((acc, current) => {
+      return acc + parseFloat(current.credito_percentage);
+    }, 0);
+    const sum_canali_percentage = result[0].reduce((acc, current) => {
+      return acc + parseFloat(current.canali_percentage);
+    }, 0);
+    const sum_finanza_percentage = result[0].reduce((acc, current) => {
+      return acc + parseFloat(current.finanza_percentage);
+    }, 0);
+    const sum_pagamenti_percentage = result[0].reduce((acc, current) => {
+      return acc + parseFloat(current.pagamenti_percentage);
+    }, 0);
+    const sum_risk_percentage = result[0].reduce((acc, current) => {
+      return acc + parseFloat(current.risk_percentage);
+    }, 0);
+
+    const average_credito_percentage = sum_credito_percentage / result[0].length;
+    const average_canali_percentage = sum_canali_percentage / result[0].length;
+    const average_finanza_percentage = sum_finanza_percentage / result[0].length;
+    const average_pagamenti_percentage = sum_pagamenti_percentage / result[0].length;
+    const average_risk_percentage = sum_risk_percentage / result[0].length;
+
+    const averages = {
+      avg_credito: average_credito_percentage,
+      avg_canali: average_canali_percentage,
+      avg_finanza: average_finanza_percentage,
+      avg_pagamenti: average_pagamenti_percentage,
+      avg_risk: average_risk_percentage
+    };
+
+    res.json(averages);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 app.post('/login', async (req, res) => {
@@ -61,11 +93,11 @@ app.post('/save-result', async (req, res) => {
         start_time,
         end_time,
         duration_in_sec,
-        first_percentage,
-        second_percentage,
-        third_percentage,
-        fourth_percentage,
-        fifth_percentage
+        credito_percentage,
+        canali_percentage,
+        finanza_percentage,
+        pagamenti_percentage,
+        risk_percentage
       ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     `;
         const result = await connection.execute(query, [
