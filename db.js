8dabcc2122a690c9672acfd2a8b79915d2998190
PEDICINO STEFANO <stefano.pedicino@esternibisp.com>
2025-07-11 17:22:08 +0200

    Modified db to Torso.io

--- db.js
+++ db.js
@@ -1,20 +1,76 @@
 // db.js
-const sqlite3 = require('sqlite3').verbose();
+import { createClient } from "@libsql/client";
 
-const dbConfig = {
-  host: 'libsql://afastdb-stefanopedicinogit.aws-eu-west-1.turso.io',
-};
+const turso = createClient({
+  url: 'libsql://afastdb-stefanopedicinogit.aws-eu-west-1.turso.io',
+  authToken: 'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJleHAiOjE3NjAwMTg2MTAsImdpZCI6IjNiYTQ0NzM1LWVmOTctNDVlNy1hYzAxLTgyOGI5NTlmOTIxOSIsImlhdCI6MTc1MjI0MjYxMCwicmlkIjoiNTQzNjRmZDQtZmM5Zi00NDg4LTgzYzQtZDdiOTg2NDc1M2M4In0.j_5TEdI7sEMPL_DWnyRANitTmeK6LvysWXQSpw3ezYyBwfSc7cbPglnPbOuUWH_vfrQbi1wOrHZpujuhLy06BA'
+});
 
-async function connect() {
+export async function retrieve_get() {
+  console.log(turso);
   try {
-    const connection = new sqlite3.Database({
-      uri: dbConfig.host,
-    });
-    return connection;
+    const query = 'SELECT * FROM afast_results';
+    const response = await turso.execute(query);
+    return response;
   } catch (error) {
     console.error('Error connecting to database:', error);
     throw error;
   }
 }
 
-module.exports = { connect };
+export async function login(enterprise_id) {
+  try {
+    const query = 'SELECT * FROM afast_results WHERE enterprise_id = ?'
+    const response = await turso.execute(query, [enterprise_id]);
+    return response;
+  }
+  catch (error) {
+    console.error('Error connecting to database:', error);
+    throw error;
+  }
+}
+
+
+export async function insert_result(enterprise_id, score, start_time, end_time, duration_in_sec, credito_percentage, canali_percentage, finanza_percentage, pagamenti_percentage, risk_percentage) {
+  try {
+    const query = `
+      INSERT INTO afast_results (
+        enterprise_id,
+        score,
+        start_time,
+        end_time,
+        duration_in_sec,
+        credito_percentage,
+        canali_percentage,
+        finanza_percentage,
+        pagamenti_percentage,
+        risk_percentage
+      ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    `;
+
+    const response = await turso.execute(query, [enterprise_id, score, start_time, end_time, duration_in_sec, credito_percentage, canali_percentage, finanza_percentage, pagamenti_percentage, risk_percentage]);
+    return response;
+  } catch (error) {
+    console.error('Error connecting to database:', error);
+    throw error;
+  }
+}
+
+export async function insert_answer(enterprise_id, domanda,risposta, categoria_predizione, ok_ko) {
+  try {
+    const query = `
+      INSERT INTO risposte (
+        E_ID,
+        Domanda,
+        Risposta_fornita,
+        Categoria_Predizione,
+        OK_KO
+      ) VALUES (?, ?, ?, ?, ?)
+    `;
+    const response = await turso.execute(query, [enterprise_id, domanda, risposta, categoria_predizione, ok_ko]);
+    return response;
+  } catch (error) {
+    console.error('Error connecting to database:', error);
+    throw error;
+  }
+}
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // server.js
 const express = require('express');
 const cors = require('cors');
-const { connect } = require('./db');
+const { retrieve_get, login, insert_result, insert_answer } = require('./db');
 
 const app = express();
 app.use(cors());
@@ -11,41 +11,37 @@ const port = 8000;
 
 app.get('/get', async (req, res) => {
   try {
-    const connection = await connect();
-    const result = await connection.all('SELECT * FROM afast_results');
+    const response = await retrieve_get();
+    const result = response.rows
+    console.log('res', result);
+    if (result.length !== null || result.length !== undefined || result.length !== 0) {
 
-    console.log(result[0]);
-    const sum_credito_percentage = result[0].reduce((acc, current) => {
-      return acc + parseFloat(current.credito_percentage);
-    }, 0);
-    const sum_canali_percentage = result[0].reduce((acc, current) => {
-      return acc + parseFloat(current.canali_percentage);
-    }, 0);
-    const sum_finanza_percentage = result[0].reduce((acc, current) => {
-      return acc + parseFloat(current.finanza_percentage);
-    }, 0);
-    const sum_pagamenti_percentage = result[0].reduce((acc, current) => {
-      return acc + parseFloat(current.pagamenti_percentage);
-    }, 0);
-    const sum_risk_percentage = result[0].reduce((acc, current) => {
-      return acc + parseFloat(current.risk_percentage);
-    }, 0);
+      const sum_credito_percentage = result.reduce((acc, current) => acc + current.credito_percentage, 0);
+      const sum_canali_percentage = result.reduce((acc, current) => acc + current.canali_percentage, 0);
+      const sum_finanza_percentage = result.reduce((acc, current) => acc + current.finanza_percentage, 0);
+      const sum_pagamenti_percentage = result.reduce((acc, current) => acc + current.pagamenti_percentage, 0);
+      const sum_risk_percentage = result.reduce((acc, current) => acc + current.risk_percentage, 0);
+      
+      const average_credito_percentage = sum_credito_percentage / result.length;
+      const average_canali_percentage = sum_canali_percentage / result.length;
+      const average_finanza_percentage = sum_finanza_percentage / result.length;
+      const average_pagamenti_percentage = sum_pagamenti_percentage / result.length;
+      const average_risk_percentage = sum_risk_percentage / result.length;
 
-    const average_credito_percentage = sum_credito_percentage / result[0].length;
-    const average_canali_percentage = sum_canali_percentage / result[0].length;
-    const average_finanza_percentage = sum_finanza_percentage / result[0].length;
-    const average_pagamenti_percentage = sum_pagamenti_percentage / result[0].length;
-    const average_risk_percentage = sum_risk_percentage / result[0].length;
+      const averages = {
+        avg_credito: average_credito_percentage,
+        avg_canali: average_canali_percentage,
+        avg_finanza: average_finanza_percentage,
+        avg_pagamenti: average_pagamenti_percentage,
+        avg_risk: average_risk_percentage
+      };
 
-    const averages = {
-      avg_credito: average_credito_percentage,
-      avg_canali: average_canali_percentage,
-      avg_finanza: average_finanza_percentage,
-      avg_pagamenti: average_pagamenti_percentage,
-      avg_risk: average_risk_percentage
-    };
-
-    res.json(averages);
+      res.status(200).json(averages);
+    }
+    else {
+      res.status(501).json({ error: 'No data found' });
+      return;
+    }
   } catch (error) {
     console.error('Error:', error);
     res.status(500).json({ error: 'Internal Server Error' });
@@ -53,108 +49,66 @@ app.get('/get', async (req, res) => {
 });
 
 app.post('/login', async (req, res) => {
-    const { enterprise_id } = req.body;
-    try {
-        const connection = await connect();
-        const result = await connection.all('SELECT * FROM afast_results WHERE enterprise_id = ?', [enterprise_id]);
+  const { enterprise_id } = req.body;
+  try {
+    const response = await login(enterprise_id);
+    const result = response.rows
+    console.log('result', result);
 
-        if (result[0].length > 0) {
-            res.status(400).json({ error: 'Enterprise ID already existing' });
-        } else {
-            res.status(200).json({ message: 'Enterprise ID is available' });
-        }
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+    if (result.length > 0) {
+      res.status(400).json({ error: 'Enterprise ID already existing' });
+    } else {
+      res.status(200).json({ message: 'Enterprise ID is available' });
     }
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 app.post('/save-result', async (req, res) => {
-    const {
-        enterprise_id,
-        score,
-        start_time,
-        end_time,
-        duration_in_sec,
-        first_percentage,
-        second_percentage,
-        third_percentage,
-        fourth_percentage,
-        fifth_percentage,
-    } = req.body;
+  const {
+    enterprise_id,
+    score,
+    start_time,
+    end_time,
+    duration_in_sec,
+    first_percentage,
+    second_percentage,
+    third_percentage,
+    fourth_percentage,
+    fifth_percentage,
+  } = req.body;
 
-    try {
-        console.log(first_percentage, second_percentage, third_percentage, fourth_percentage, fifth_percentage)
-        const connection = await connect();
-        const query = `
-      INSERT INTO afast_results (
-        enterprise_id,
-        score,
-        start_time,
-        end_time,
-        duration_in_sec,
-        credito_percentage,
-        canali_percentage,
-        finanza_percentage,
-        pagamenti_percentage,
-        risk_percentage
-      ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    `;
-        const result = await connection.run(query, [
-            enterprise_id,
-            score,
-            start_time,
-            end_time,
-            duration_in_sec,
-            first_percentage,
-            second_percentage,
-            third_percentage,
-            fourth_percentage,
-            fifth_percentage,
-        ]);
-
-        res.json({ message: 'Result saved successfully' });
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+  try {
+    const response = await insert_result(enterprise_id, score, start_time, end_time, duration_in_sec, first_percentage, second_percentage, third_percentage, fourth_percentage, fifth_percentage);
+    const result = response.rows
+    res.status(200).json({ message: 'Result saved successfully' });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 app.post('/save-answers', async (req, res) => {
-    const {
-        enterprise_id,
-        domanda,
-        risposta,
-        categoria_predizione,
-        ok_ko
-    } = req.body;
+  const {
+    enterprise_id,
+    domanda,
+    risposta,
+    categoria_predizione,
+    ok_ko
+  } = req.body;
 
-    try {
-        const connection = await connect();
-        const query = `
-      INSERT INTO risposte (
-        E_ID,
-        Domanda,
-        Risposta_fornita,
-        Categoria_Predizione,
-        OK_KO
-      ) VALUES (?, ?, ?, ?, ?)
-    `;
-        const result = await connection.run(query, [
-            enterprise_id,
-            domanda,
-            risposta,
-            categoria_predizione,
-            ok_ko
-        ]);
-
-        res.json({ message: 'Result saved successfully' });
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+  try {
+    const response = await insert_answer(enterprise_id, domanda, risposta, categoria_predizione, ok_ko);
+    const result = response.rows
+    res.json({ message: 'Result saved successfully' });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
